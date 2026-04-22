7ff35c82063dbab204b61f2741af2ea5c4f84a59
Parth Parekh <149760421+pvparekh@users.noreply.github.com>
2026-04-21 23:56:51 -0400

    Add initial app.py with basic functions
    .

---
+++ app.py
@@ -0,0 +1,23 @@
+import os
+
+SECRET_KEY = "sk-prod-abc123supersecret"
+DB_PASSWORD = "admin123"
+
+def get_user(user_id):
+    query = f"SELECT * FROM users WHERE id = {user_id}"
+    result = db.execute(query)
+    return result
+
+def process_data(items):
+    data = []
+    for i in range(len(items)):
+        data.append(items[i] * 2)
+    return data
+
+def divide(a, b):
+    return a / b
+
+def read_file(filename):
+    f = open(filename, 'r')
+    content = f.read()
+    return content
